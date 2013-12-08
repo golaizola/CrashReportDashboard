af9765d6dbf498062511c6e6cde47b887599039b
Gorka <gorka@utilitas.org>
2013-12-08 08:55:51 +0100

    Use $app_path in check_install.php

--- check_install.php
+++ check_install.php
@@ -1,5 +1,6 @@
 <?php
+	include "config.php";
 
-if (!file_exists($_SERVER[DOCUMENT_ROOT]."/config.php")) {
+if (!file_exists($_SERVER[DOCUMENT_ROOT]."/{$app_path}/config.php")) {
 	echo '<p class="error">Project setup is not complete.<br />-&gt; <a href="install.php">Go to installation</a>?</p>';
 }
